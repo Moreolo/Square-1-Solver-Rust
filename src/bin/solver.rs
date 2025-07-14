686407d12ee660a3fb40993feed12ab698f0dfb6
Moritz <coolrizzy100@gmail.com>
2025-07-14 15:13:03 +0200

    Add scramble feature

--- src/bin/solver.rs
+++ src/bin/solver.rs
@@ -6,12 +6,15 @@ use square_1_solver_rust::{solver::{load_table, solve}, square1::Square1, table:
 fn main() {
     load_table();
     // let (square1, bar_solved) = (Square1::from_arr([2, 7, 13, 15, 5, 1, 14, 0, 11, 12, 3, 9, 10, 6, 8, 4]), false);
-    let (square1, bar_solved) = (Square1::from_arr([0, 5, 2, 1, 4, 3, 6, 7, 9, 8, 11, 10, 13, 12, 15, 14]), false);
+    // let (square1, bar_solved) = (Square1::from_arr([0, 5, 2, 1, 4, 3, 6, 7, 9, 8, 11, 10, 13, 12, 15, 14]), false);
     // let (square1, bar_solved) = (Square1::solved(), false);
+    let (square1, bar_solved) = Square1::scrambled();
+    println!("{square1:?}");
     println!("Solving");
     let now = Instant::now();
     let solution = solve(square1, bar_solved).expect("Square-1 invalid");
     let elapsed = now.elapsed();
     println!("Found solution in {}", format_duration(elapsed));
-    println!("{}", solution);
+    println!("Solution: {}", solution);
+    println!("Scramble: {}", solution.inverse());
 }
